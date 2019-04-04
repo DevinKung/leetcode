2ab797932e18c4fd3c31b57212ccc511bcf0a41c
DevinKung <37767769+DevinKung@users.noreply.github.com>
2019-04-04 14:52:53 +0800

    Update 94.py

--- 94.py
+++ 94.py
@@ -1,6 +1,9 @@
 中序遍历的顺序为左-根-右
 栈的思想
+Simple, understandable solution. In the loop: If we get a node with flag false, we add children in correct order and set them to false. because they have to be processed (for their children). And we set flag of current node to true.
 
+If we get node with flag set to true we simply print its value (add to acc).
+Can be modified to do post and pre order too.
 class Solution:
     def inorderTraversal(self, root):
         stack = [ (False, root) ]
