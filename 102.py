afe0658f970cba3924ef487157bd37dd93b8941b
DevinKung <37767769+DevinKung@users.noreply.github.com>
2019-04-04 15:08:49 +0800

    Update 102.py

--- 102.py
+++ 102.py
@@ -4,3 +4,19 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        if not root:
+            return res
+        
+        q = [root]
+        while q:
+            r = [c.val for c in q]
+            res.append(r)
+            qq = []
+            for c in q:
+                if c.left: qq.append(c.left)
+                if c.right: qq.append(c.right)
+            q = qq
+        return res
